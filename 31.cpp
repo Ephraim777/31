513cddb3d6013bc6224544f8d7e8ce9ce3aa890d
Ephraim777 <165553036+Ephraim777@users.noreply.github.com>
2024-08-13 22:29:53 -0700

    Update 31.cpp

--- 31.cpp
+++ 31.cpp
@@ -3,29 +3,40 @@
 using namespace std;
 
 template<typename T>
-void square(T& value) {
-    value = value * value;
+void square(T& value) 
+{
+value = value * value;
 }
 
 template<typename T>
-void square(std::vector<T>& values) {
-    for (auto& val : values) {
+void square(std::vector<T>& values)
+{
+    for (auto& val : values)
+     {
         val = val * val;
-    }
+     }
 }
 
-int main() {
+int main()
+{
     int a = 6;
+    
     square(a);
+    
     cout << "Squared value of a: " << a << endl;
-
+    
     std::vector<int> vec = {1, 2, 3, 4, 5};
+    
     square(vec);
+    
     cout << "Squared values of vector: ";
-    for (const auto& val : vec) {
+    
+    for (const auto& val : vec)
+    {
         cout << val << " ";
     }
+    
     cout << endl;
-
+    
     return 0;
 }
